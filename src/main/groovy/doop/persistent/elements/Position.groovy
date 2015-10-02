2dacda2515b7064722fe84d5803f9b1669da6971
Anastasios Antoniadis <anastasios.antoniadis@gmail.com>
2015-10-02 16:06:52 +0300

    Additions to Position class

--- src/main/groovy/doop/persistent/elements/Position.groovy
+++ src/main/groovy/doop/persistent/elements/Position.groovy
@@ -10,4 +10,17 @@ class Position extends ItemImpl{
     private long startColumn;
     private long endLine;
     private long endColumn;
+
+    public Position(long startLine, long startColumn, long endColumn) {
+        this.startLine = startLine;
+        this.startColumn = startColumn;
+        this.endColumn = endColumn;
+    }
+
+    public Position(long startLine, long endLine, long startColumn, long endColumn) {
+        this.startLine = startLine;
+        this.endLine = endLine;
+        this.startColumn = startColumn;
+        this.endColumn = endColumn;
+    }
 }
