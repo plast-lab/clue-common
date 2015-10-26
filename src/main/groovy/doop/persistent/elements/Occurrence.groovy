b13eb7f9c6ae4cb13b69d4604f9ef8c4a762cddf
Anastasios Antoniadis <anastasios.antoniadis@gmail.com>
2015-10-26 06:46:57 +0200

    Changes in Occurrence

--- src/main/groovy/doop/persistent/elements/Occurrence.groovy
+++ src/main/groovy/doop/persistent/elements/Occurrence.groovy
@@ -3,20 +3,20 @@ package doop.persistent.elements
 /**
  * Created by anantoni on 2/10/2015.
  */
-class Occurrence<T extends Symbol> {
+class Occurrence {
 
     Position position;
-    T symbol;
-    OccurrenceType occurrenceType;
     String sourceFileName;
+    String symbolID;
+    OccurrenceType occurrenceType;
 
     public Occurrence() {}
 
-    public Occurrence(Position position, String sourceFileName, T symbol, OccurrenceType occurrenceType) {
+    public Occurrence(Position position, String sourceFileName, String symbolID, OccurrenceType occurrenceType) {
         this.position = position;
-        this.symbol = symbol;
-        this.occurrenceType = occurrenceType;
         this.sourceFileName = sourceFileName;
+        this.symbolID = symbolID;
+        this.occurrenceType = occurrenceType;
     }
 }
 
