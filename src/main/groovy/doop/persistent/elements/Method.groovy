9de91b2719828c03f74acb8d8ae676b432ca471b
Anastasios Antoniadis <anantoni@di.uoa.gr>
2016-04-05 22:43:42 +0300

    Add totalAllocations and totalInvocations fields to Method, change constructor

--- src/main/groovy/doop/persistent/elements/Method.groovy
+++ src/main/groovy/doop/persistent/elements/Method.groovy
@@ -13,11 +13,14 @@ class Method extends Symbol {
     String[] params;
     String[] paramTypes;
     boolean isStatic;
+    int totalInvocations;
+    int totalAllocations;
 
     public Method() {}
 
     public Method(Position position, String sourceFileName, String name, String declaringClassID, String returnType,
-                  String doopSignature, String doopCompactName, String[] params, String[] paramTypes, boolean isStatic)
+                  String doopSignature, String doopCompactName, String[] params, String[] paramTypes, boolean isStatic,
+                  totalInvocations, totalAllocations)
     {
         super(position, sourceFileName);
         this.name = name;
@@ -28,5 +31,7 @@ class Method extends Symbol {
         this.params = params;
         this.paramTypes = paramTypes;
         this.isStatic = isStatic;
+        this.totalInvocations = totalInvocations;
+        this.totalAllocations = totalAllocations;
     }
 }
