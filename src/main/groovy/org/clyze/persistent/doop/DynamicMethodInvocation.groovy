f9eda8638dd6b043e65dc3b0fb2bb9e165925f4c
George Fourtounis <gfour@di.uoa.gr>
2017-05-25 16:14:21 +0300

    Add single generator for invokedynamic IDs (to be used by jcPlugin, Jimple parser, and Doop's Representation.)

---
+++ src/main/groovy/org/clyze/persistent/doop/DynamicMethodInvocation.groovy
@@ -0,0 +1,15 @@
+package org.clyze.persistent.doop
+
+class DynamicMethodInvocation {
+    /**
+     * Creates an instruction ID for an invokedynamic that calls a
+     * lambda metafactory, passing it a method handle c::meth.
+     *
+     * @param c        method handle: class name
+     * @param meth     method handle: method name
+     * return          the invokedynamic ID
+     */
+    static String genId(String c, String meth) {
+	return "invokedynamic_${c}::${meth}"
+    }
+}
