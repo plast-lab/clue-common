afb3ebd4537ab66a16c99a2c3979912dc4dd138f
George Kastrinis <george.kast@gmail.com>
2017-05-26 12:46:12 +0300

    Remove store module (will move to server)

--- src/main/groovy/org/clyze/persistent/store/OrderBy.groovy
+++
@@ -1,9 +0,0 @@
-package org.clyze.persistent.store
-
-/**
- * Created by saiko on 30/11/2016.
- */
-class OrderBy {
-    String field
-    Ordering ordering = Ordering.ASC
-}
--- src/main/groovy/org/clyze/persistent/store/Ordering.groovy
+++
@@ -1,9 +0,0 @@
-package org.clyze.persistent.store
-
-/**
- * Created by saiko on 30/11/2016.
- */
-enum Ordering {
-    ASC,
-    DESC
-}
