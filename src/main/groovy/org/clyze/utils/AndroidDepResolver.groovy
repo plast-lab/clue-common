9036b40f996a9e00c3129efceec23efe161cbb5b
George Fourtounis <gfour@di.uoa.gr>
2017-10-24 00:11:57 +0300

    Control if toJars() ignores non-JAR/AAR inputs.

--- src/main/groovy/org/clyze/utils/AndroidDepResolver.groovy
+++ src/main/groovy/org/clyze/utils/AndroidDepResolver.groovy
@@ -1,7 +1,5 @@
 package org.clyze.utils
 
-import java.nio.file.*;
-
 // This class takes care of resolving dependencies on Android, using
 // the local Android SDK and maven.org. Before calling resolution, you
 // must call once findSDK(rootDir), to set the SDK path from property
@@ -242,66 +240,6 @@ class AndroidDepResolver {
             }
         }
         return ret
-    }
-
-    // Decompress AAR and find its classes.jar. Any other .jar entries
-    // are locally extracted and their paths are pushed into 'extraJars'.
-    private static void unpackClassesJarFromAAR(File localAAR, String targetJar,
-                                                Set<String> extraJars) {
-        boolean classesJarFound = false
-        def zipFile = new java.util.zip.ZipFile(localAAR)
-        zipFile.entries().each {
-            if (it.name.equals('classes.jar')) {
-                File cj = new File(targetJar)
-                cj.newOutputStream() << zipFile.getInputStream(it)
-                classesJarFound = true
-            } else if (it.name.endsWith(".jar")) {
-                // Extract to temporary directory & add to return set.
-                File ej = new File(createTmpDir() + "/" + basename(it.name, ""))
-                ej.newOutputStream() << zipFile.getInputStream(it)
-                extraJars << ej.canonicalPath
-            }
-        }
-        if (!classesJarFound) {
-            String aarPath = localAAR.getCanonicalPath()
-            throwRuntimeException("No classes.jar found in ${aarPath}")
-        }
-    }
-
-    private static String createTmpDir() {
-        Path tmpDir = Files.createTempDirectory("aar")
-        tmpDir.toFile().deleteOnExit()
-        return tmpDir.toString()
-    }
-
-    // Transforms a set of Java archives: JAR archives are returned,
-    // while AARs are searched for JAR entries, which are returned.
-    public static List<String> toJars(List<String> archives) {
-        List<String> jars = []
-        archives.each { String ar ->
-            if (ar.endsWith(".jar")) {
-                jars << ar
-            } else if (ar.endsWith(".aar")) {
-                String jar = createTmpDir() + "/" + basename(ar, ".aar") + ".jar"
-                Set<String> extraJars = new HashSet<>()
-                unpackClassesJarFromAAR(new File(ar), jar, extraJars)
-                println "Extracted ${jar} from ${ar}"
-                jars << jar
-                if (extraJars.size() > 0) {
-                    println "Extracted ${extraJars} from ${ar}"
-                    jars.addAll(extraJars)
-                }
-            } else {
-                println "Ignoring file of unknown type: ${ar}"
-            }
-        }
-        return jars
-    }
-
-    private static String basename(String path, String ext) {
-        int sIdx = path.lastIndexOf(File.separator)
-        int extSz = ext.length()
-        return path.substring(sIdx == -1 ? 0 : sIdx, path.length() - extSz)
     }
 
     // Resolves an Android dependency by finding its .aar/.jar in the
