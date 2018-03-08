1ed12fbc0b96b3572368fd878767e68288d112a8
Tony Antoniadis <anastasios.antoniadis@gmail.com>
2018-03-08 18:54:03 +0200

    Change preprocessor handling of strings passed with -D

--- src/main/groovy/org/clyze/utils/CPreprocessor.groovy
+++ src/main/groovy/org/clyze/utils/CPreprocessor.groovy
@@ -22,10 +22,8 @@ class CPreprocessor {
 		.collect { AnalysisOption option ->
 			if (option.value instanceof Boolean)
 				"-D${option.id}" as String
-			else if (option.value instanceof Integer)
-				"-D${option.id}=${option.value}" as String
 			else
-				"-D${option.id}='\"${option.value}\"'" as String
+				"-D${option.id}=${option.value}" as String
 		}
 		this.executor = executor
 		emitLineMarkers = false
