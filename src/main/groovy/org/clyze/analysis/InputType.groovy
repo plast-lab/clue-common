75968b0a95097e4fd4106217e3b936bd6922774d
George Fourtounis <gfour@di.uoa.gr>
2018-04-12 15:32:33 +0300

    Fix problem in InputType initialization.

--- src/main/groovy/org/clyze/analysis/InputType.groovy
+++ src/main/groovy/org/clyze/analysis/InputType.groovy
@@ -3,15 +3,15 @@ package org.clyze.analysis
 import static ResolverType.*
 
 public final enum InputType {
-    INPUT("input", LOCAL_FILE, URL, MAVEN_ARTIFACT),
-    LIBRARY("library", LOCAL_FILE, URL, MAVEN_ARTIFACT),
-    HPROF("heap dump", LOCAL_FILE, URL, ZIP),
-    DYNAMIC("dynamic class", LOCAL_FILE, ZIP)
+    INPUT("input", [LOCAL_FILE, URL, MAVEN_ARTIFACT]),
+    LIBRARY("library", [LOCAL_FILE, URL, MAVEN_ARTIFACT]),
+    HPROF("heap dump", [LOCAL_FILE, URL, ZIP]),
+    DYNAMIC("dynamic class", [LOCAL_FILE, ZIP])
 
     private final String title
     private final Set<ResolverType> resolvers
 
-    public InputType(String title, ResolverType[] resolvers) {
+    public InputType(String title, List<ResolverType> resolvers) {
         this.title = title
         this.resolvers = resolvers as Set
     }
