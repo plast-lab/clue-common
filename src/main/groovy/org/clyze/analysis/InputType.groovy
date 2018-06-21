c8f36d5126b98d2a8071bb33a4754507999fcf6e
George Kastrinis <george.kast@gmail.com>
2018-06-21 20:00:29 +0300

    Add PLATFORM to possible input types

--- src/main/groovy/org/clyze/analysis/InputType.groovy
+++ src/main/groovy/org/clyze/analysis/InputType.groovy
@@ -3,6 +3,7 @@ package org.clyze.analysis
 enum InputType {
     INPUT("input", [ResolverType.LOCAL_FILE, ResolverType.URL, ResolverType.MAVEN_ARTIFACT]),
     LIBRARY("library", [ResolverType.LOCAL_FILE, ResolverType.URL, ResolverType.MAVEN_ARTIFACT]),
+    PLATFORM("platform", [ResolverType.LOCAL_FILE, ResolverType.URL, ResolverType.ZIP]),
     HPROF("heap dump", [ResolverType.LOCAL_FILE, ResolverType.URL, ResolverType.ZIP]),
 
     private final String title
