998810990d038ca99039b16dfcf0f7d69aff1d2b
George Kastrinis <george.kast@gmail.com>
2018-07-12 13:10:05 +0300

    Attempt to fix flushing of monitor file

--- src/main/groovy/org/clyze/utils/Executor.groovy
+++ src/main/groovy/org/clyze/utils/Executor.groovy
@@ -110,47 +110,48 @@ class Executor {
 		def pid = fld.get(process)
 
 		def monitorFile = new File(currWorkingDir, "monitoring.txt")
-		def monitorWriter = monitorFile.newWriter()
 		def monitorFileLatest = new File(currWorkingDir, "monitoring.latest.txt")
-		log.info "Runtime info monitored in $monitorFile.absolutePath"
-		monitorWriter.writeLine "$monitoringInterval"
-		while (process.alive) {
-			startProcess("top -b -n 1 -p $pid".split().toList()).inputStream.newReader().withReader { reader ->
-				// If pid is still valid (e.g. process has not ended) the last line has the actual information
-				def lastLine = reader.readLines().last()
-				if (lastLine.startsWith(pid as String)) {
-					def parts = lastLine.split()
-					def values = [
-							"PID"      : parts[0],
-							"USER"     : parts[1],
-							"PR"       : parts[2],
-							"NI"       : parts[3],
-							"VIRT"     : parts[4],
-							"RES"      : parts[5],
-							"SHR"      : parts[6],
-							"S"        : parts[7],
-							"CPU_PER"  : parts[8],
-							"MEM_PER"  : parts[9],
-							"TIME_PLUS": parts[10],
-							"COMMAND"  : parts[11]
-					]
+		log.info "Runtime info monitored in $monitorFile.absolutePath (& $monitorFileLatest.absolutePath)"
+		monitorFile.withWriter { writer ->
+			writer.writeLine "$monitoringInterval"
+			while (process.alive) {
+				startProcess("top -b -n 1 -p $pid".split().toList()).inputStream.newReader().withReader { reader ->
+					// If pid is still valid (e.g. process has not ended) the last line has the actual information
+					def lastLine = reader.readLines().last()
+					if (lastLine.startsWith(pid as String)) {
+						def parts = lastLine.split()
+						def values = [
+								"PID"      : parts[0],
+								"USER"     : parts[1],
+								"PR"       : parts[2],
+								"NI"       : parts[3],
+								"VIRT"     : parts[4],
+								"RES"      : parts[5],
+								"SHR"      : parts[6],
+								"S"        : parts[7],
+								"CPU_PER"  : parts[8],
+								"MEM_PER"  : parts[9],
+								"TIME_PLUS": parts[10],
+								"COMMAND"  : parts[11]
+						]
 
-					// If RES ends with "g" it's measured in GB, with "t" in TB, with "m" in MB, otherwise in KB. Convert to MB.
-					if (values.RES.endsWith("g")) values.RES = values.RES[0..-2].toDouble() * 1024
-					else if (values.RES.endsWith("t")) values.RES = values.RES[0..-2].toDouble() * 1024 * 1024
-					else if (values.RES.endsWith("m")) values.RES = values.RES[0..-2].toDouble()
-					else values.RES = values.RES.toDouble() / 1024
-					def info = "${values.PID}\t${values.RES.toLong()}MB\t${values.CPU_PER.toDouble()}\t${values.COMMAND}"
+						// If RES ends with "g" it's measured in GB, with "t" in TB, with "m" in MB, otherwise in KB. Convert to MB.
+						if (values.RES.endsWith("g")) values.RES = values.RES[0..-2].toDouble() * 1024
+						else if (values.RES.endsWith("t")) values.RES = values.RES[0..-2].toDouble() * 1024 * 1024
+						else if (values.RES.endsWith("m")) values.RES = values.RES[0..-2].toDouble()
+						else values.RES = values.RES.toDouble() / 1024
+						def info = "${values.PID}\t${values.RES.toLong()}MB\t${values.CPU_PER.toDouble()}\t${values.COMMAND}"
 
-					monitorWriter.writeLine info
-					// Delete previous contents
-					monitorFileLatest.withWriter { it.writeLine info }
+						writer.writeLine info
+						// Delete previous contents
+						monitorFileLatest.withWriter { it.writeLine info }
 
-					monitorClosure?.call values
+						monitorClosure?.call values
+					}
+					null
 				}
-				null
+				Thread.currentThread().sleep(monitoringInterval)
 			}
-			Thread.currentThread().sleep(monitoringInterval)
 		}
 	}
 }
