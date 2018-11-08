65ecea3f57ad6dc73e6a9dff3e9748e04b189b03
George Fourtounis <gfour@di.uoa.gr>
2018-11-08 13:52:16 +0200

    Add generic invokedynamic id generator.

--- src/main/java/org/clyze/persistent/model/doop/DynamicMethodInvocation.java
+++ src/main/java/org/clyze/persistent/model/doop/DynamicMethodInvocation.java
@@ -1,15 +1,26 @@
 package org.clyze.persistent.model.doop;
 
 public class DynamicMethodInvocation {
-	/**
-	 * Creates an instruction ID for an invokedynamic that calls a
-	 * lambda metafactory, passing it a method handle c::meth.
-	 *
-	 * @param c method handle: class name
-	 * @param meth method handle: method name
-	 * return          the invokedynamic ID
-	 */
-	public static String genId(String c, String meth) {
-		return "invokedynamic_" + c + "::" + meth;
-	}
+    /**
+     * Creates an instruction ID for an invokedynamic that calls a
+     * lambda metafactory, passing it a method handle c::meth.
+     *
+     * @param c     method handle: class name
+     * @param meth  method handle: method name
+     * return       the invokedynamic ID
+     */
+    public static String genId(String c, String meth) {
+        return "invokedynamic_" + c + "::" + meth;
+    }
+
+    /**
+     * Creates an instruction ID for a generic invokedynamic.
+     *
+     * @param       the simple name of the boot method
+     * @param       the simple name of the dynamic method
+     * return       the invokedynamic ID
+     */
+    public static String genericId(String bootName, String dynamicName) {
+        return "invokedynamic_" + bootName + "::" + dynamicName;
+    }
 }
