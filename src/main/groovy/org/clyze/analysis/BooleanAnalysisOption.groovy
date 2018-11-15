ce3457c9fc02e5d946f21871b2f4b70df737c589
Kostas Saidis <saikos@gmail.com>
2018-11-15 19:24:31 +0200

    Fix default value of boolean options (from null, to false)

--- src/main/groovy/org/clyze/analysis/BooleanAnalysisOption.groovy
+++ src/main/groovy/org/clyze/analysis/BooleanAnalysisOption.groovy
@@ -3,4 +3,9 @@ package org.clyze.analysis
 import groovy.transform.InheritConstructors
 
 @InheritConstructors
-class BooleanAnalysisOption extends AnalysisOption<Boolean> {}
+class BooleanAnalysisOption extends AnalysisOption<Boolean> {
+	/**
+	 * The value of the option
+	 */
+	Boolean value = false
+}
