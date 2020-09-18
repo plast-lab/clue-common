8891eb63154f7d0ddc2773db9897d92ff6aef400
George Fourtounis <gfour@di.uoa.gr>
2020-09-18 12:05:39 +0300

    Fix class visibility.

--- src/main/java/org/clyze/utils/OS.java
+++ src/main/java/org/clyze/utils/OS.java
@@ -1,6 +1,6 @@
 package org.clyze.utils;
 
-class OS {
+public class OS {
     public static final boolean macOS = System.getProperty("os.name").toLowerCase().contains("mac");
     public static final boolean win   = System.getProperty("os.name").toLowerCase().contains("windows");
     public static final boolean linux = System.getProperty("os.name").toLowerCase().contains("linux");
